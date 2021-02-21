bae93af672b4ec5369dd4ca9b3164e2ce20eda05
Nazar Velychko <nazar.vel@gmail.com>
2021-02-21 09:41:53 +0200

    FileCombiner.js docs ready

--- classes/FileCombiner.js
+++ classes/FileCombiner.js
@@ -1,7 +1,5 @@
+/**@module FileCombiner*/
 const fs = require("fs")
-const { colorText } = require("../utils")
-const { startWorkWithRawConsole, endWorkWithRawConsole } = require("../utils")
-
 const {
 	initProgressBar,
 	askQuestion,
@@ -10,19 +8,50 @@ const {
 	filterFilesByType,
 	fileTypeFormat,
 	ignoreSpecialNamedFiles,
-	errorToText
+	errorToText,
+	startWorkWithRawConsole,
+	endWorkWithRawConsole,
+	colorText
 } = require("../utils")
 
+/**Class combining list of files with specified type in one with \n between them
+ * @class
+ * @requires ...utils:utils/FileCombinerUtils
+ * @requires fs*/
 class FileCombiner {
+	/**
+	 * Name of end file
+	 * @type {string}
+	 * @private
+	 * */
 	#endFileName = "_united.fna"
+
+	/**
+	 * Wanted file type
+	 * @type {string}
+	 * @private
+	 * */
 	#fileType
+
+	/**
+	 * Path of folder with specified filetype
+	 * @type {string}
+	 * @private
+	 * */
 	#filesFolderPath
 
+	/** Init of settings
+	 * @param {string} filesFolderPath
+	 * @param {string} fileType
+	 * */
 	constructor(filesFolderPath, fileType) {
 		this.#filesFolderPath = filesFolderPath
 		this.#fileType = fileTypeFormat(fileType)
 	}
 
+	/** Program stops until previous end file will be deleted
+	 * @async
+	 * */
 	async savePreviousEndFile() {
 		const endFilePath = `${this.#filesFolderPath}\\${this.#endFileName}`
 
@@ -42,6 +71,10 @@ class FileCombiner {
 		}
 	}
 
+	/** Sequential call of each file to be read and append to main file
+	 * @param {string[]} files - filenames in destination folder
+	 * @async
+	 * */
 	async combineFiles(files) {
 		await this.savePreviousEndFile()
 
@@ -71,6 +104,9 @@ class FileCombiner {
 		endWorkWithRawConsole()
 	}
 
+	/** Main class method
+	 * @async
+	 */
 	async start() {
 		try {
 			const files = getFilesFromFolder(this.#filesFolderPath)
