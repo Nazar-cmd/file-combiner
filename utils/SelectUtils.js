f9bd3f4990b34d2a27cbb79a68c416a0d9d20fe2
Nazar Velychko <nazar.vel@gmail.com>
2021-02-21 10:02:34 +0200

    SelectUtils.js docs ready

--- utils/SelectUtils.js
+++ utils/SelectUtils.js
@@ -1,16 +1,31 @@
 const rdl = require("readline")
+/**@module SelectUtils
+ * @requires rdl:readline*/
 
+/** Shorting for process.stdout
+ * @const {Object}
+ */
 const stdout = process.stdout
+
+/** Shorting for process.stdin
+ * @const {Object}
+ */
 const stdin = process.stdin
 
+/**Function shows cursor in console*/
 function showCursor() {
 	stdout.write("\x1B[?25h")
 }
 
+/**Function hides cursor in console*/
 function hideCursor() {
 	stdout.write("\x1B[?25l")
 }
 
+/** Function colors text in specified color
+ * @param {string} str
+ * @param {string} [colorName]
+ * */
 function colorText(str, colorName = "yellow") {
 	const colors = {
 		yellow: [33, 89],
@@ -27,23 +42,34 @@ function colorText(str, colorName = "yellow") {
 	return start + str + stop
 }
 
+/**Clears screen from specified position
+ * @param {number} startY - position, from which start to clear screen
+ * */
 function clearScreen(startY) {
 	rdl.cursorTo(stdout, 0, startY)
 	rdl.clearScreenDown(stdout)
 }
 
+/**Writes text on specified y position in console
+ * @param {number} lineNum
+ * @param {string} text
+ * */
 function writeOnLine(lineNum, text) {
 	rdl.cursorTo(stdout, 0, lineNum)
 	rdl.clearLine(stdout, 0)
 	stdout.write(text)
 }
 
+/** Getting window height and width
+ * @returns {object} Object with height and width keys
+ * */
 function getWindowSize() {
 	const [width, height] = stdout.getWindowSize()
 
 	return { width, height }
 }
 
+/** Setting console in raw mode*/
 function startWorkWithRawConsole() {
 	stdin.setRawMode(true)
 	stdin.resume()
@@ -51,6 +77,7 @@ function startWorkWithRawConsole() {
 	hideCursor()
 }
 
+/**Setting console back to norma and deleting 'data' and 'pause' listeners*/
 function endWorkWithRawConsole() {
 	stdin.setRawMode(false)
 	stdin.pause()
